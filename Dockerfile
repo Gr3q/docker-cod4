49d880abafc47cb877e70da079ab27734e04fcdb
Attila Greguss <floyd0122@gmail.com>
2023-11-17 09:34:52 +0000

    Update with codx4 mod v17.6.2

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
-FROM centos:7
+FROM ubuntu:22.04
 
 #from http://cod4-linux-server.webs.com/
-RUN yum update -y
-RUN yum -y install glibc.i686 libgcc.i686 libstdc++.i686 zlib.i686
+
+
+RUN dpkg --add-architecture i386
+RUN apt-get update && apt-get upgrade -y
+RUN apt-get install gcc-multilib  -y
 
 RUN useradd cod4
 ADD cod4 /home/cod4/
@@ -10,4 +13,4 @@ RUN chown -R cod4:cod4 /home/cod4
 
 USER cod4
 WORKDIR /home/cod4
-ENTRYPOINT /home/cod4/start.sh
+CMD [ "/home/cod4/start.sh" ]
--- cod4/start.sh
+++ cod4/start.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-/home/cod4/cod4_lnxded \
-+set dedicated "${SERVERTYPE:-1}"\
-+set net_port "${PORT:-28960}"\
-+set net_ip "${IP:-0.0.0.0}"\
-+set sv_authorizemode "${AUTHORIZEMODE:--1}\
-+set sv_maxclients "${MAXCLIENTS:-32}"\
-+set rcon_password "${RCONPASSWORD:-changeme}"\
-+set sv_punkbuster "${PUBNKBUSTER:-1}"\
-+set g_password "${PASSWORD:-changeme}"\
-+exec "${CONFIG_FILE:-server.cfg}\
-+map_rotate
+/home/cod4/cod4_lnxded +set dedicated "${SERVERTYPE:-1}" +set net_port "${PORT:-28960}" +set net_ip "${IP:-0.0.0.0}" +set sv_authorizemode "0" +set sv_maxclients "${MAXCLIENTS:-32}" +set rcon_password "${RCONPASSWORD:-changeme}" +set sv_punkbuster "${PUBNKBUSTER:-1}" +set g_password "${PASSWORD:-changeme}" +exec "${CONFIG_FILE:-server.cfg}" +map_rotate
