cd014c12e4083603401a68e047d55451efb48ac4
Attila Greguss <attila.greguss@netduma.com>
2024-06-28 13:04:05 +0100

    add FS_GAME env variable support

--- cod4/start.sh
+++ cod4/start.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-/home/cod4/cod4_lnxded +set dedicated "${SERVERTYPE:-1}" +set net_port "${PORT:-28960}" +set net_ip "${IP:-0.0.0.0}" +set sv_authorizemode "0" +set sv_maxclients "${MAXCLIENTS:-32}" +set rcon_password "${RCONPASSWORD:-changeme}" +set sv_punkbuster "${PUBNKBUSTER:-1}" +set g_password "${PASSWORD:-changeme}" +exec "${CONFIG_FILE:-server.cfg}" +map_rotate
+
+# build the start command
+
+command="/home/cod4/cod4_lnxded"
+command+=" +set dedicated ${SERVERTYPE:-1}"
+command+=" +set net_port ${PORT:-28960}"
+command+=" +set net_ip ${IP:-0.0.0.0}"
+command+=" +set sv_authorizemode 0"
+if [ -n "$FS_GAME" ]; then
+    command+=" +set fs_game $FS_GAME"
+fi
+
+command+=" +set sv_maxclients ${MAXCLIENTS:-32}"
+command+=" +set rcon_password ${RCONPASSWORD:-changeme}"
+command+=" +set sv_punkbuster ${PUBNKBUSTER:-1}"
+command+=" +set g_password ${PASSWORD:-changeme}"
+command+=" +exec ${CONFIG_FILE:-server.cfg}"
+command+=" +map_rotate"
+
+# run the command
+eval "$command"
